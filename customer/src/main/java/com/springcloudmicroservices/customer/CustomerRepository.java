430301e4fde43d6c1320bbd445b951f922c9a6d0
Omair Iqbal <omair95@protonmail.com>
2021-12-01 13:41:40 +0000

    Customer microservice added which is connected to a PosgresSQL DB

---
+++ customer/src/main/java/com/springcloudmicroservices/customer/CustomerRepository.java
@@ -0,0 +1,7 @@
+package com.springcloudmicroservices.customer;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CustomerRepository extends JpaRepository<Customer, Integer> {
+
+}
---
+++ customer/src/main/java/com/springcloudmicroservices/customer/CustomerService.java
@@ -0,0 +1,23 @@
+package com.springcloudmicroservices.customer;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomerService {
+
+    @Autowired
+    CustomerRepository customerRepository;
+
+    public void registerCustomer(CustomerRegistrationRequest request) {
+        Customer customer = Customer.builder()
+                .firstName(request.getFirstName())
+                .lastName(request.getLastName())
+                .email(request.getEmail())
+                .build();
+        // TODO: check if email valid
+        // TODO: check if email not taken
+
+        customerRepository.save(customer);
+    }
+}
